aecce2ebc89541b0f1ec86c62a5811868757aabf
EgidijusGrysa <grysa123@gmail.com>
2018-04-16 23:43:49 +0100

    added to get all user meals

--- routes/food.js
+++ routes/food.js
@@ -7,6 +7,22 @@ var Food = require('../models/food');
 var DayMeal = require('../models/dayMeals');
 var UserMeal = require('../models/userMeal');
 
+router.get('/healthapp/usermeals', function (req, res, next) {
+    UserMeal.find()
+        .exec(function (err, messages) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: messages
+            });
+        });
+});
+
 router.post('/healthapp/meals', function(req,res,next){
     console.log(req.body.dayMeal.eveMeal);
         var userMeal = new UserMeal({
